998d5d528f323c5e765aef778209bf95bcb501bd
abhishek-bits <abhi.shar1971@gmail.com>
2024-05-26 15:17:57 +0530

    initial commit

---
+++ src/main/java/com/contact_reconciliation/ContactReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.contact_reconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactReconciliationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contact_reconciliation/ContactReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact_reconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
